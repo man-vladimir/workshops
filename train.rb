c81a6f5777443b357cdcff131d0baae05b30b8c6
Vladimir Manaenko <manaenko1@gmail.com>
2022-02-17 11:51:38 +0200

    Refactor and optimization code

--- train.rb
+++ train.rb
@@ -1,42 +1,52 @@
 # https://www.codecademy.com/courses/learn-ruby/lessons/a-night-at-the-movies/exercises/nice-work-1
 
-movies = {Matrix: 4, Alien: 4, Emmanuel: 5 }
-movies.each {|movie, rating| puts "#{movie}: #{rating}\n"}
-puts "What would you like to do?"
-choice = gets.chomp
-case choice
-  when 'add'
-    puts 'What movie do you want to add?'
-    title = gets.chomp
-    puts "What's the rating? (Type a number 0 to 4.)"
-    rating = gets.chomp
-    if movies[title.to_sym].nil?
-      movies[title.to_sym] = rating.to_i
-    else 
-      puts 'this movie already exist!'
-    end
-  when 'update'
-    puts 'What movie do you want to update?'
-    title = gets.chomp
-    if movies[title.to_sym].nil?
-      puts 'This movie absent from your movies'
-    else
-       puts "What's the new rating? (Type a number 0 to 4.)"
-      rating = gets.chomp
-      movies[title.to_sym] = rating.to_i     
-    end
-  when 'display'
-    movies.each {|movie, rating| puts "#{movie}: #{rating}"}
-  when 'delete'
-    puts 'What movie do you want to delete?'
-    title = gets.chomp
-    if movies[title.to_sym].nil?
-      puts 'This movie absent from your movies'
-    else
-      movies.delete(title.to_sym)
-      puts movies
-    end
-  else
-   puts 'Error!'
+def gets_to_sym
+  gets.chomp.to_sym
 end
+
+def gets_to_i
+  gets.chomp.to_i
+end
+
+def print_all_movies(movies)
+  movies.each { |movie, rating| puts "#{movie}: #{rating}"}
+end
+
+def my_movies
+
+  movies = { Matrix: 4, Alien: 4, Emmanuel: 5 }
+
+  puts 'What would you like to do?'
+  choice = gets.chomp
+
+  case choice
+    when 'add'
+      puts 'What movie do you want to add?'
+      title = gets_to_sym
+      return puts 'this movie already exist!' if movies[title]
+
+      puts "What's the rating? (Type a number 0 to 4.)"
+      movies[title] = gets_to_i
+    when 'update'
+      puts 'What movie do you want to update?'
+      title = gets_to_sym
+      return puts 'This movie absent from your movies' unless movies[title]
+
+      puts "What's the new rating? (Type a number 0 to 4.)"
+      movies[title] = gets_to_i 
+    when 'display'
+      print_all_movies(movies)
+    when 'delete'
+      puts 'What movie do you want to delete?'
+      title = gets_to_sym
+      return puts 'This movie absent from your movies' unless movies[title]
+      movies.delete(title)
+      print_all_movies(movies)
+    else puts 'Error!'
+  end
+
+end
+
+my_movies()
+
 
