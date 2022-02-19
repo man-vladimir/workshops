d10841de5b5b6839b3ab98af7f85ea5223d52e30
Vladimir Manaenko <manaenko1@gmail.com>
2022-02-19 20:16:26 +0200

    Added code use case lambda

---
+++ lambda.rb
@@ -0,0 +1,16 @@
+# https://www.codecademy.com/courses/learn-ruby/lessons/blocks-procs-and-lambdas/exercises/passing-your-lambda-to-a-method
+
+crew = {
+    captain: "Picard",
+    first_officer: "Riker",
+    lt_cdr: "Data",
+    lt: "Worf",
+    ensign: "Ro",
+    counselor: "Troi",
+    chief_engineer: "LaForge",
+    doctor: "Crusher"
+}
+ 
+first_half = lambda { |key, value| value < "M"}
+a_to_m = crew.select(&first_half)
+puts a_to_m
