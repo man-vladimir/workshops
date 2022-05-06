097fa29602442ebd767b4a37e0a2d2d31b3c63cd
Vladimir Manaenko <manaenko1@gmail.com>
2022-05-06 14:09:03 +0300

    Added codeacademy OOP workout

---
+++ computer.rb
@@ -0,0 +1,27 @@
+# https://www.codecademy.com/courses/learn-ruby/lessons/virtual-computer/exercises/you-did-it-1
+
+class Computer
+
+  @@users = {}
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{@username} create file #{filename} at #{@files[filename]}"
+  end
+  
+  def Computer.get_users
+    return @@users
+  end
+end
+
+my_computer = Computer.new('work_123', 12345)
+my_computer.create('Resume')
+
