b58c0a3b208e03bdbfc90929f7e273724936316e
Vladimir Manaenko <manaenko1@gmail.com>
2022-05-10 16:56:07 +0300

    Refactor code

--- account.rb
+++ account.rb
@@ -1,48 +1,48 @@
 # https://www.codecademy.com/courses/learn-ruby/lessons/banking-on-ruby/exercises/well-done
 
-class Account 
-
-    attr_reader :name
-    attr_reader :balance
+class Account     
 
     def initialize(name, balance=100)
       @name = name
       @balance = balance
     end
 
+    def display_balance(pin_number)
+
+      puts pin_number == pin ?  "Balance: $#{@balance}. Your account: #{name}":  pin_error
+       
+    end
+
+    def withdraw(pin_number, amount)
+
+      return puts pin_error if pin_number != pin
+      return puts "There are not enough funds in your account #{name}" if amount > balance
+
+      self.balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{balance}."      
+        
+    end   
+    
+    def deposite(pin_number, amount)
+
+      return puts pin_error if pin_number != pin        
+        self.balance += amount
+      puts "Received on account #{name} $#{amount}. New balance: $#{balance}."
+     
+    end
+
     private
+
+    attr_reader :name
+    attr_accessor :balance
+
     def pin
       @pin = 1234
     end
 
     def pin_error
-      return 'Access denied: incorrect PIN.'
-    end
-
-    public      
-      def display_balance(pin_number)
-
-         puts pin_number == pin ?  "Balance: $#{@balance}. Your account: #{@name}":  pin_error
-          
-      end
-
-      def withdraw(pin_number, amount)
-
-        return puts pin_error if pin_number != pin
-        return puts "There are not enough funds in your account #{@name}" if amount > @balance
-
-        @balance -= amount
-        puts "Withdrew #{amount}. New balance: $#{@balance}."      
-          
-      end     
-
-      def deposite(pin_number, amount)
-
-        return puts pin_error if pin_number != pin        
-        @balance += amount
-        puts "Received on account #{@name} $#{amount}. New balance: $#{@balance}."
-       
-      end
+      'Access denied: incorrect PIN.'
+    end     
 end
 checking_account = Account.new('my_secret_millions', 1_000_000_000)
 checking_account.display_balance(1234)
